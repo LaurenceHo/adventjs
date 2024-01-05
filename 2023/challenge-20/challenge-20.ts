c021fd73600d110e7f6d2c8ffe7140a67a08cfc7
Laurence Ho <bluegray.he@aol.com>
2024-01-06 11:17:04 +1300

    My solution for challenge #20

--- 2023/challenge-20/challenge-20.ts
+++ 2023/challenge-20/challenge-20.ts
@@ -1,27 +1,62 @@
 export function distributeGifts(weights: Array<Array<number | null>>) {
-  const averages: number[][] = []
+  const distributedWeight = [];
 
-  for (const [i, row] of weights.entries()) {
-    averages[i] = []
+  for (let row = 0; row < weights.length; row++) {
+    const line = weights[row];
+    const tempWeightLine = [];
+    for (let column = 0; column < line.length; column++) {
+      let countNode = 0;
+      let selfNode = line[column];
+      if (selfNode !== null) {
+        countNode++;
+      } else {
+        selfNode = 0;
+      }
 
-    for (const [j, col] of row.entries()) {
-      const nextCol = row[j + 1]
-      const prevCol = row[j - 1]
-      const prevRow = weights[i - 1]?.[j]
-      const nextRow = weights[i + 1]?.[j]
-      const values = [col, prevCol, nextCol, prevRow, nextRow]
+      let rightNode: number | null = 0;
+      let leftNode: number | null = 0;
+      let topNode: number | null = 0;
+      let bottomNode: number | null = 0;
+      if (column < line.length - 1) {
+        rightNode = weights[row][column + 1];
+        if (rightNode !== null) {
+          countNode++;
+        } else {
+          rightNode = 0;
+        }
+      }
 
-      const total = values.reduce<number>((acc, value) => acc + (value ?? 0), 0)
-      const divisor = values.reduce<number>(
-        (acc, value) => acc + +(value != null),
-        0,
-      )
+      if (column > 0) {
+        leftNode = weights[row][column - 1];
+        if (leftNode !== null) {
+          countNode++;
+        } else {
+          leftNode = 0;
+        }
+      }
 
-      const average = Math.round(total / divisor)
+      if (row > 0) {
+        topNode = weights[row - 1][column];
+        if (topNode !== null) {
+          countNode++;
+        } else {
+          topNode = 0;
+        }
+      }
 
-      averages[i][j] = average
+      if (row < weights.length - 1) {
+        bottomNode = weights[row + 1][column];
+        if (bottomNode !== null) {
+          countNode++;
+        } else {
+          bottomNode = 0;
+        }
+      }
+
+      const average = Math.round((selfNode + topNode + bottomNode + rightNode + leftNode) / countNode);
+      tempWeightLine.push(average);
     }
+    distributedWeight.push(tempWeightLine);
   }
-
-  return averages
+  return distributedWeight;
 }
