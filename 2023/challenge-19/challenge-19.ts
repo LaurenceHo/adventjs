74063104c2407b96a4590f8770e02cf9171cb857
Laurence Ho <bluegray.he@aol.com>
2024-01-06 12:30:29 +1300

    My solution for challenge #19

--- 2023/challenge-19/challenge-19.ts
+++ 2023/challenge-19/challenge-19.ts
@@ -1,32 +1,62 @@
 export function revealSabotage(store: string[][]) {
-  for (const [i, row] of store.entries()) {
-    for (const [j, col] of row.entries()) {
-      if (col === '*') continue
+  const result = JSON.parse(JSON.stringify(store));
+  const calculateNode = (node: string | number) => {
+    if (node !== '*') {
+      if (node === ' ') {
+        node = 1;
+      } else {
+        node = Number(node) + 1;
+      }
+    }
+    return node;
+  };
 
-      const nextRow = store[i + 1]
-      const prevRow = store[i - 1]
+  for (let row = 0; row < result.length; row++) {
+    const line = result[row];
+    for (let column = 0; column < line.length; column++) {
+      if (line[column] === '*') {
+        if (column < line.length - 1) {
+          const rightNode = result[row][column + 1];
+          result[row][column + 1] = String(calculateNode(rightNode));
 
-      const adjacentCells = [
-        prevRow?.[j - 1],
-        prevRow?.[j],
-        prevRow?.[j + 1],
-        row[j - 1],
-        row[j + 1],
-        nextRow?.[j - 1],
-        nextRow?.[j],
-        nextRow?.[j + 1],
-      ]
+          if (row < result.length - 1) {
+            const bottomRight = result[row + 1][column + 1];
+            result[row + 1][column + 1] = String(calculateNode(bottomRight));
+          }
 
-      const count = adjacentCells.reduce(
-        (acc, curr) => acc + +(curr === '*'),
-        0,
-      )
+          if (row > 0) {
+            const topRight = result[row - 1][column + 1];
+            result[row - 1][column + 1] = String(calculateNode(topRight));
+          }
+        }
 
-      if (count !== 0) {
-        row[j] = `${count}`
+        if (column > 0) {
+          const leftNode = result[row][column - 1];
+          result[row][column - 1] = String(calculateNode(leftNode));
+
+          if (row < result.length - 1) {
+            const bottomLeft = result[row + 1][column - 1];
+            result[row + 1][column - 1] = String(calculateNode(bottomLeft));
+          }
+
+          if (row > 0) {
+            const topLeft = result[row - 1][column - 1];
+            result[row - 1][column - 1] = String(calculateNode(topLeft));
+          }
+        }
+
+        if (row > 0) {
+          const topNode = result[row - 1][column];
+          result[row - 1][column] = String(calculateNode(topNode));
+        }
+
+        if (row < result.length - 1) {
+          const bottomNode = result[row + 1][column];
+          result[row + 1][column] = String(calculateNode(bottomNode));
+        }
       }
     }
   }
 
-  return store
+  return result;
 }
