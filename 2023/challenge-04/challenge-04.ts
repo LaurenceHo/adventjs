a49f41a5b0b159771d3fd93dd03dc25633b56ac7
Laurence Ho <bluegray.he@aol.com>
2024-01-06 10:19:04 +1300

    My solution for challenge #4

--- 2023/challenge-04/challenge-04.ts
+++ 2023/challenge-04/challenge-04.ts
@@ -1,5 +1,31 @@
 export function decode(message: string) {
-  const regexp = /\(([^()]*)\)/g
-  const replacer = (_: string, r: string) => [...r].reverse().join('')
-  return message.replace(regexp, replacer).replace(regexp, replacer)
+  let reversedMessage = message;
+  const reverse = (sliceString: string) => {
+    const splitString = sliceString.split('');
+    const reverseArray = splitString.reverse();
+
+    return reverseArray.join('');
+  };
+
+  const findBracketArray = (m: string) => {
+    const regex = /\(([^()]+)\)/g;
+    return m.match(regex) || [];
+  };
+
+  const composeMessage = (bracketArray: string[]) => {
+    bracketArray.forEach((s) => {
+      const removeBracket = s.substring(1, s.length - 1);
+      const reverseString = reverse(removeBracket);
+      reversedMessage = reversedMessage.replace(s, reverseString);
+    });
+  };
+
+  composeMessage(findBracketArray(message));
+
+  if (findBracketArray(reversedMessage).length > 0) {
+    composeMessage(findBracketArray(reversedMessage));
+    return reversedMessage;
+  } else {
+    return reversedMessage;
+  }
 }
